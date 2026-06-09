415d51d64025a46d4594c2d30850d0bd0ae32f9a
Ron Gustafsson <ron.gustafsson@gmail.com>
2026-06-09 09:32:05 +0300

    Osa 3 C: Tietojen tallettaminen MongoDB-tietokantaan valmis

--- fullstackopen/osa3/puhelinluettelo_backend/index.js
+++ fullstackopen/osa3/puhelinluettelo_backend/index.js
@@ -25,114 +25,149 @@ app.use(
   morgan(
     ':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-      name: "Arto Hellas",
-      number: "040-123456",
-      id: "1"
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: "2"
-    },
-    {
-      name: "Dan Abramov",
-      number: "12-43-234345",
-      id: "3"
-    },
-    {
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-      id: "4"
-    }
-]
-
-// step 1 palauttaa persons taulukon henkilöt json-muodossa, express muuntaa automaattisesti
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
+// 3.13: haetaan kaikki henkilöt MongoDB-tietokannasta
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+      .then(persons => {
         response.json(persons)
-    })
+      })
+      .catch(error => next(error))
 })
 
-// step 2 palautetaan persons henkilömäärä ja pyynnön tekohetki
-app.get('/info', (request, response) => {
-    const henkiloMaara = persons.length
-    const pyyntoAika = new Date()
+// 3.18 muutoksen jälkeen hakee henkilöiden määrän mongodb-tietokannasta
+app.get('/info', (request, response, next) => {
+  Person.countDocuments({})
+    .then(count => {
+      const pyyntoAika = new Date()
 
-    response.send(`
-      <p>Phonebook has info for ${henkiloMaara} people</p>
-      <p>${pyyntoAika}</p>
+      response.send(`
+        <p>Phonebook has info for ${count} people</p>
+        <p>${pyyntoAika}</p>
       `)
+    })
+    .catch(error => next(error))
 })
 
-// step 3 yksittäisen puhelinnumerotiedon näyttäminen
-app.get(`/api/persons/:id`, (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person) // palauttaa henkilön
-  } else {
-    response.status(404).end() // jos ei löyd not found
-  }
+// 3.18: yksittäisen puhelintiedon näyttäminen tietokannasta
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-//step 4 yksittäisen tiedon poisto
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  // luo uuden taulukon ilman poistettavaa henkilöä
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+// 3.15: yksittäisen puhelintiedon poisto MongoDB-tietokannasta
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-//step 5 uuden puhelintiedon lisäys
-app.post('/api/persons', (request, response) => {
+// 3.14: uuden puhelintiedon lisäys MONGODB tietokantaan
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  // step 6 virheenkäsittely nimi ja numero
+  // tarkistetaan että nimi on annettu
   if (!body.name) {
     return response.status(400).json({
       error: 'name missing'
     })
   }
 
+  // tarkistetaan että numero on annettu
   if (!body.number) {
     return response.status(400).json({
       error: 'number missing'
     })
   }
 
-  // jos nimi esiintyy jo
-  const nameExists = persons.some(person => person.name === body.name)
-
-    if (nameExists) {
-      return response.status(400).json({
-        error: 'name must be unique'
-      })
-    }
-
-  const id = Math.floor(Math.random() * 1000000 + 1).toString() // uusi id
-
-  // tehdään uusi henkilö
-  const newPerson = {
+  // luodaan uusi henkilö MongoDB varten
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: id,
+  })
+
+  // tallentaa henkilön tietokantaan
+  person.save()
+    .then(savedPerson => {
+      // palauttaa uuden henkilön
+      response.status(201).json(savedPerson)
+      console.log("Henkilö tallennettu onnistuneesti")
+    })
+    .catch(error => next(error))
+})
+
+// 3.17: päivitetään olemassa olevan henkilön numero MongoDB-tietokantaan
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  // nimen tarkastus
+  if (!name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
   }
 
-  // lisää uuden henkilön persons taulukkoon
-  persons = persons.concat(newPerson)
-  // palauttaa uuden henkilön
-  response.status(201).json(newPerson)
+  // numeron tarkastus
+  if (!number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  Person.findById(request.params.id)
+    .then(person => {
+      // jos henkilöä ei löydy id:llä
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      // päivitetään henkilön tiedot
+      person.name = name
+      person.number = number
+
+      // tallennetaan muutos tietokantaan
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
+
+// 3.16: tuntematton osoitteen käsittely
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({
+    error: 'unknown endpoint'
+  })
+}
+
+app.use(unknownEndpoint)
+
+// 3.16: keskitetty virheidenkäsittely middlewareen
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  }
+  // muut virheet Expressille
+  next(error)
+}
+
+// kutsutaan virheenkäsittelijää
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001 // step 10: käyttöön ympäristömuuttujan portti (.env) tai 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-// 3.12: 6 tuntia
-// 3.13 
